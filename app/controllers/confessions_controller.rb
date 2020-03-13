64b601ee627c704093dc70211978a1d9ea790576
Iliana <ip97@cornell.edu>
2020-03-13 00:12:31 +0000

    Create confession button creates confession in database

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -1,9 +1,19 @@
 class ConfessionsController < ApplicationController
 	def index 
-		@confession = Confession.all
+		@confession = Confession.all.reverse
 	end
 
 	def new
 		@confession = Confession.new
+  	end 
+
+  	def create
+  		Confession.create(confession_params) 
+  		redirect_to root_path
+  	end
+
+  	private 
+  	def confession_params
+  		params.require(:confession).permit(:saying, :author)
   	end
 end
