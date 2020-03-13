a23237c835a9061f4d172f5543e3b685ba8dc008
Iliana <ip97@cornell.edu>
2020-03-13 00:55:09 +0000

    validate form, submit button works, added error messages

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -8,8 +8,14 @@ class ConfessionsController < ApplicationController
   	end 
 
   	def create
-  		Confession.create(confession_params) 
-  		redirect_to root_path
+  		@confession = Confession.create(confession_params) 
+  		if @confession.invalid?
+  			flash[:error] = 'Couldn\'t save. The saying you entered is invalid. Make sure it\'s between 3-500 characters'
+  			redirect_to new_confession_path
+  		end
+  		if @confession.valid? 
+  			redirect_to root_path
+  		end
   	end
 
   	private 
--- app/models/confession.rb
+++ app/models/confession.rb
@@ -1,2 +1,4 @@
 class Confession < ApplicationRecord
+	validates :saying, presence: true, length: {maximum: 500, minimum: 3}
+	validates :author, presence: true, length: {maximum: 20}
 end
